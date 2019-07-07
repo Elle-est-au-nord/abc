9d1a6778a152fe14a786c2cb9eb7e158445e436f
Eleonore9 <eleonore.mayola@ymail.com>
2019-07-07 23:44:49 +0200

    Add an intro, repo link and legend

--- sketch.js
+++ sketch.js
@@ -6,30 +6,31 @@ function preload() {
 
 function setup() {
   // createCanvas(900, 900);
-  createCanvas(windowWidth - 500, windowHeight - 100);
+  createCanvas(windowWidth - 800, windowHeight - 200);
   noStroke();
 }
+
+function drawTitle(subtitle, x_pos, y_pos){
+  fill("#adc5e7");
+  text(subtitle, x_pos, y_pos - 40);
+  fill("#006d6f");
+  text(subtitle, x_pos, y_pos - 38);
+  fill("#fcd3c1");
+  text(subtitle, x_pos, y_pos - 36);
+}
+
+let legendEn = "";
+let legendFr = "";
 
 function draw() {
   let x_coord_en = 0;
   let x_coord_fr = 450;
   let y_coord = 70;
   background("#808080");
-  // title English
+  // titles
   textSize(30);
-  fill("#adc5e7");
-  text('English', 30, y_coord - 40);
-  fill("#006d6f");
-  text('English', 30, y_coord - 38);
-  fill("#fcd3c1");
-  text('English', 30, y_coord - 36);
-  // title French
-  fill("#adc5e7");
-  text('French', x_coord_fr + 30, y_coord - 40);
-  fill("#006d6f");
-  text('French', x_coord_fr + 30, y_coord - 38);
-  fill("#fcd3c1");
-  text('French', x_coord_fr + 30, y_coord - 36);
+  drawTitle('English', 30, y_coord);
+  drawTitle('French', x_coord_fr + 30, y_coord);
 
   // Iterate through CSV data
   for (i=0; i<table.getRowCount(); i++){
@@ -44,6 +45,16 @@ function draw() {
     side_en = freq_en * 10;
     side_fr = freq_fr * 10;
     fill(table.get(i, 'colour'));
+
+    // add to the legend
+    if (!legendEn.includes(letter)){
+	legendEn += letter + ": ";
+	legendEn += parseFloat(freq_en).toFixed(1) + "% ";
+    }
+    if (!legendFr.includes(letter)){
+	legendFr += letter + ": ";
+	legendFr += parseFloat(freq_fr).toFixed(1) + "% ";
+    }
 
     if (i != 0 && i % 6 == 0){
       x_coord_en = 0;
@@ -67,6 +78,9 @@ function draw() {
     x_coord_en = x_coord_en + side_en + 10;
     x_coord_fr = x_coord_fr + side_fr + 10;
   }
+    textSize(13);
+    text(legendEn, 30, 620, 400, 70);
+    text(legendFr, x_coord_fr, 620, 400, 70);
 }
 
 function windowResized() {
